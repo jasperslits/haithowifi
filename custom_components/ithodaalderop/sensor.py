6030721877e8530f41294b812f91c665d94d990c
Jasper Slits <jasper@slits.nl>
2024-11-09 18:02:03 +0000

    Fix set up of remotes and rooms

--- custom_components/ithodaalderop/sensor.py
+++ custom_components/ithodaalderop/sensor.py
@@ -60,11 +60,9 @@ def _create_autotemprooms(config_entry: ConfigEntry):
     cfg = config_entry.data
     configured_sensors = []
     for x in range(1, 8):
-        _LOGGER.warn("Range " + str(x))
         template_sensors = copy.deepcopy(list(AUTOTEMPSENSORS))
         room = cfg["room" + str(x)]
         if room != "" and room != "Room " + str(x):
-            _LOGGER.warn("Room " + str(x))
             for sensor in template_sensors:
                 sensor.key = f"{MQTT_BASETOPIC["autotemp"]}/{MQTT_STATETOPIC["autotemp"]}"
                 sensor.json_field = sensor.json_field.replace("X", str(x))
@@ -88,15 +86,15 @@ async def async_setup_entry(
             description.key = f"{MQTT_BASETOPIC["noncve"]}/{MQTT_STATETOPIC["noncve"]}"
             sensors.append(IthoSensor(description, config_entry, AddOnType.NONCVE))
 
-        (sensors.append(IthoSensor(description, config_entry, AddOnType.REMOTES)) for description in _create_remotes(config_entry))
-
     if config_entry.data[CONF_ADDON_TYPE] == "cve":
         for description in CVESENSORS:
             description.key = f"{MQTT_BASETOPIC["cve"]}/{MQTT_STATETOPIC["cve"]}"
             sensors.append(IthoSensor(description, config_entry, AddOnType.CVE))
 
     if config_entry.data[CONF_ADDON_TYPE] in ["cve","noncve"]:
-            sensors.append(IthoSensor(description, config_entry, AddOnType.REMOTES) for description in _create_remotes(config_entry))
+        for description in _create_remotes(config_entry):
+            description.key = f"{MQTT_BASETOPIC[config_entry.data[CONF_ADDON_TYPE]]}/{MQTT_STATETOPIC["remotes"]}"
+            sensors.append(IthoSensor(description, config_entry, AddOnType.REMOTES) )
 
     if config_entry.data[CONF_ADDON_TYPE] == "wpu":
         for description in WPUSENSORS:
@@ -104,7 +102,9 @@ async def async_setup_entry(
             sensors.append(IthoSensor(description, config_entry, AddOnType.WPU))
 
     if config_entry.data[CONF_ADDON_TYPE] == "autotemp":
-        sensors.append(IthoSensor(description, config_entry, AddOnType.AUTOTEMP) for description in _create_autotemprooms(config_entry))
+        for description in _create_autotemprooms(config_entry):
+            description.key = f"{MQTT_BASETOPIC["autotemp"]}/{MQTT_STATETOPIC["autotemp"]}"
+            sensors.append(IthoSensor(description, config_entry, AddOnType.AUTOTEMP))
 
     async_add_entities(sensors)
 
