beef60327e9b4052784e52761f82e4cf68d2d314
benjamin-dcs <78026082+benjamin-dcs@users.noreply.github.com>
2024-12-26 10:09:31 +0100

    Fix autotemp common range
    The reason I didn't like this structure and choose for the explicit tuple 😋

--- custom_components/ithodaalderop/sensors/autotemp.py
+++ custom_components/ithodaalderop/sensors/autotemp.py
@@ -34,7 +34,7 @@ def get_autotemp_sensors(config_entry: ConfigEntry):
     """Create sensors for Autotemp."""
     sensors = []
     topic = f"{MQTT_BASETOPIC["autotemp"]}/{MQTT_STATETOPIC["autotemp"]}"
-    for i in range(1, 12):
+    for i in range(1, 13):
         letter = chr(i + 64)
         description = copy.deepcopy(AUTOTEMP_COMM_SPACE_SENSOR_TEMPLATE)
         description.topic = topic
